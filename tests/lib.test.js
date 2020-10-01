65db530113a9708dcac0fbffbf4319e2af7ca5bb
Souma <souma.hitech@gmail.com>
2020-10-01 20:43:18 +0530

    add test for getCurrencies function.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -24,3 +24,31 @@ describe('greet', () => {
         expect(result).toContain('Souma');
     });
 });
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+       const result = lib.getCurrencies(); 
+
+       //too general
+       expect(result).toBeDefined();
+       expect(result).not.toBeNull();
+
+       //to specific
+       expect(result[0]).toBe('USD');
+       expect(result[1]).toBe('AUD');
+       expect(result[2]).toBe('EUR');
+       expect(result.length).toBe(3);
+
+       //proper way
+       expect(result).toContain('USD');
+       expect(result).toContain('AUD');
+       expect(result).toContain('EUR');
+       
+       //ideal way
+       expect(result).toEqual(expect.arrayContaining(['EUR','AUD','USD']));
+       
+
+
+    });
+    
+});
